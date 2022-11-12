2fcf22fdef36972bc572aa95446ffb9f061b4bd8
midoliy <midoliy@outlook.jp>
2022-11-12 23:36:40 +0900

    wip

--- ext/js/background.js
+++ ext/js/background.js
@@ -1,16 +1,20 @@
+/* pattern 1 */
+const port = chrome.runtime.connectNative("jp.midoliy.app");
+port.onMessage.addListener((req) => {
+  console.log('msg:', req)
+})
+port.onDisconnect.addListener((req) => {
+  console.log('dosconnect:', req)
+})
 chrome.runtime.onMessage.addListener(function (msg) {
-
-  /* pattern 1 */
-  const port = chrome.runtime.connectNative("jp.midoliy.app");
-  port.onMessage.addListener((req) => {
-    console.log('msg:', req)
-  })
   port.postMessage(msg);
-
-  /* pattern 2 */
-  // chrome.runtime.sendNativeMessage('jp.midoliy.app',
-  //   msg,
-  //   function (response) {
-  //     console.log("msg:", response);
-  //   });
 });
+
+// chrome.runtime.onMessage.addListener(function (msg) {
+//   /* pattern 2 */
+//   // chrome.runtime.sendNativeMessage('jp.midoliy.app',
+//   //   msg,
+//   //   function (response) {
+//   //     console.log("msg:", response);
+//   //   });
+// });
--- src/nmsg.console/Program.fs
+++ src/nmsg.console/Program.fs
@@ -1,8 +1,18 @@
-﻿open System.Windows.Forms
+﻿//// Receiving data sent from Edge extensions
+//let msg = receive()
+//MessageBox.Show(msg) |> ignore
 
-// Receiving data sent from Edge extensions
-let msg = receive()
-MessageBox.Show(msg) |> ignore
+//// Send data to Edge extensions
+//send """{ "x": 100 }"""
 
-// Send data to Edge extensions
-send """{ "x": 100 }"""
+open System.Text.Json
+type Data = { text: string }
+
+while true do
+  // Receiving data sent from Edge extensions
+  let msg = receive()
+  let json = JsonSerializer.Deserialize<Data>(msg)
+
+  // Send data to Edge extensions
+  send $"""{{ "now": "{System.DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss")}", "msg": "{json.text}" }}"""
+
