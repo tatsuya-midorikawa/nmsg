f2af0ee162e14abefe12226eda03e3fd990192c5
midoliy <midoliy@outlook.jp>
2022-11-25 08:29:03 +0900

    wip

--- ext/js/background.js
+++ ext/js/background.js
@@ -1,11 +1,15 @@
 /* pattern 1 */
+// Edge 起動時にネイティブ アプリを起動
 const port = chrome.runtime.connectNative("jp.midoliy.app");
+// ネイティブ アプリからメッセージが来た時の処理
 port.onMessage.addListener((req) => {
   console.log('msg:', req)
 })
+// ネイティブ アプリとの通信が終了した時の処理
 port.onDisconnect.addListener((req) => {
   console.log('dosconnect:', req)
 })
+// content.js (またはコンテンツ) からメッセージが来た時の処理
 chrome.runtime.onMessage.addListener(function (msg) {
   port.postMessage(msg);
 });
